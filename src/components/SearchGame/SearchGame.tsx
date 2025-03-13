036bfdd21195a2bbbf36f9d89abb5d9baecb50ae
Makus14 <mesha.maksim14@gmail.com>
2025-03-13 12:22:34 +0300

    position image, boxInfo, textInput, title

--- src/components/SearchGame/SearchGame.tsx
+++ src/components/SearchGame/SearchGame.tsx
@@ -2,9 +2,16 @@ import classes from "./SearchGame.module.css";
 
 const SearchGame = () => {
   return (
-    <>
+    <div className={classes.container}>
       <div className={classes.backgroundImage}></div>
-    </>
+      <div className={classes.boxInfo}>
+        <p className={classes.title}>Введите уникальный код заказа</p>
+        <input
+          className={classes.textInput}
+          placeholder="Введите уникальный код"
+        />
+      </div>
+    </div>
   );
 };
 
