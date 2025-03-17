304a4bf68b21c9e6445c378cba2cb74d30e84386
Makus14 <mesha.maksim14@gmail.com>
2025-03-17 10:22:29 +0300

    some changes

--- src/components/CheckingProfile/CheckingProfile.tsx
+++ src/components/CheckingProfile/CheckingProfile.tsx
@@ -1,6 +1,7 @@
 import { useMediaQuery } from "react-responsive";
 import GameComponent from "./GameComponent/GameComponent";
 import ProfileComponent from "./ProfileComponent/ProfileComponent";
+import appLogo from "../../assets/appLogoWhite.svg";
 import classes from "./CheckingProfile.module.css";
 
 const CheckingProfile = () => {
@@ -8,6 +9,9 @@ const CheckingProfile = () => {
 
   return (
     <div className={classes.container}>
+      <div className={classes.boxLogo}>
+        <img src={appLogo} alt="App Logo" width={50} height={50} />
+      </div>
       <GameComponent isMobile={isMobile} />
       <ProfileComponent isMobile={isMobile} />
     </div>
--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,5 +1,21 @@
+import appLogo from "../assets/appLogoWhite.svg";
+
 const ErrorPage = () => (
-  <div>
+  <div
+    style={{
+      display: "flex",
+      width: "100vw",
+      height: "100vh",
+      flexDirection: "column",
+      justifyContent: "center",
+      alignItems: "center",
+      backgroundColor: "#161616",
+      color: "white",
+    }}
+  >
+    <div style={{ position: "absolute", top: "5px", left: "50%" }}>
+      <img src={appLogo} alt="App Logo" width={50} height={50} />
+    </div>
     <h2>Страница не найдена</h2>
     <p>Пожалуйста, вернитесь на главную страницу.</p>
   </div>
